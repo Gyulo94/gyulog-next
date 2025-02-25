a9062f1fd38a1dbf251274a5c1bd32869ea67283
Gyulo94 <qkrcjfrb112@gmail.com>
2025-02-25 20:27:24 +0900

    외부 이미지 불러오기기

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.patterns.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "miro.medium.com",
+      },
+      {
+        protocol: "https",
+        hostname: "b1490832.smushcdn.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
