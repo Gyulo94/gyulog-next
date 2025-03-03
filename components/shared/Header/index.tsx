7d57e18392d2fe6c486ffe0f1a2e48dcd0771263
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-04 03:32:56 +0900

    Update 카테고리 위치 변경

--- components/shared/Header/index.tsx
+++ components/shared/Header/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+import logo from "@/public/images/logo.png";
+import Image from "next/image";
 import Link from "next/link";
 import ChatbotButton from "./chatbot-button";
 import { ModeToggle } from "./mode-toggle";
@@ -5,13 +8,19 @@ import { NavMenu } from "./nav-menu";
 
 export default function Header() {
   return (
-    <header className="container mx-auto py-4 px-8">
-      <div className="flex justify-between items-center">
-        <Link href="/">
-          <h1>Gyulog</h1>
+    <header className="w-full border-b z-9">
+      <div className="container mx-auto py-4 px-8 h-20 w-full flex justify-between items-center">
+        <NavMenu />
+        <Link href={"/"}>
+          <Image
+            src={logo}
+            width={24}
+            height={24}
+            alt="logo"
+            className="cursor-pointer"
+          />
         </Link>
         <div className="flex items-center space-x-4">
-          <NavMenu />
           <ModeToggle />
           <ChatbotButton />
         </div>
