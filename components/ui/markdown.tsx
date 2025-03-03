bb3754c6af0dc87b5facd8bdf2ead5678a57fd44
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-04 07:53:29 +0900

    Update 모드에 따라 뷰어 모드도 바뀜

--- components/ui/markdown.tsx
+++ components/ui/markdown.tsx
@@ -1,14 +1,19 @@
 "use client";
 import type { MarkdownPreviewProps } from "@uiw/react-markdown-preview";
 import "@uiw/react-markdown-preview/markdown.css";
+import { useTheme } from "next-themes";
 import dynamic from "next/dynamic";
 
 const MDViewer = dynamic(() => import("@uiw/react-markdown-preview"), {
   ssr: false,
 });
 
-export const MarkdownViewer = ({ ...rest }: MarkdownPreviewProps) => (
-  <div>
-    <MDViewer {...rest} />
-  </div>
-);
+export const MarkdownViewer = ({ ...rest }: MarkdownPreviewProps) => {
+  const { theme } = useTheme();
+
+  return (
+    <div data-color-mode={`${theme === "dark" ? "dark" : "light"}`}>
+      <MDViewer {...rest} />
+    </div>
+  );
+};
