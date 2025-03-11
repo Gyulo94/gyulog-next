e80b6e4e873e44b0551635c5b78a60681e388bca
Gyulo94 <qkrcjfrb112@gmail.com>
2025-03-12 01:53:09 +0900

    마무으리

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,48 +1,49 @@
-export const APP_NAME = "Gyulog";
-export const APP_DESCRIPTION = "찰규의 숨겨진 블로그입니다.";
-export const BASE_URL = "http://localhost:3000";
-export const SERVER_URL = "http://localhost:8000";
+export const APP_NAME = process.env.APP_NAME;
+export const APP_DESCRIPTION = process.env.APP_DESCRIPTION;
+export const BASE_URL = process.env.BASE_URL;
+export const SERVER_URL = process.env.SERVER_URL;
 
 export const category: { title: string; href: string; description: string }[] =
   [
     {
       title: "언어",
       href: "/category/language",
-      description: "컴퓨터와 소통하기 위한 언어.",
+      description: "개발자가 컴퓨터와 소통하는 언어에 대한 이야기",
     },
     {
       title: "프론트엔드",
       href: "/category/frontend",
-      description: "웹 애플리케이션의 사용자 인터페이스를 구성하는 파트.",
+      description: "프론트 개발자의 핵심적인 이야기",
     },
     {
       title: "백엔드",
       href: "/category/backend",
-      description:
-        "웹 애플리케이션의 서버, 데이터베이스, 애플리케이션 로직 등을 포함하여 클라이언트와의 상호작용을 지원하는 파트.",
+      description: "백엔드 개발자의 핵심적인 이야기",
     },
     {
       title: "데이터베이스",
       href: "/category/database",
-      description:
-        "데이터를 체계적으로 저장하고 관리하여 효율적인 검색과 처리를 가능하게 하는 시스템.",
+      description: "백엔드 개발자라면 꼭 알아야 할 데이터베이스에 대한 이야기",
     },
     {
       title: "OS",
       href: "/category/os",
-      description:
-        "컴퓨터 자원을 관리하고 사용자와 프로그램이 시스템을 효율적으로 사용할 수 있도록 지원하는 소프트웨어.",
+      description: "나의 개인 홈 서버에 대한 이야기",
     },
     {
       title: "CS",
       href: "/category/cs",
-      description:
-        "컴퓨터와 정보 시스템의 이론, 개발, 응용을 연구하는 학문 분야.",
+      description: "IT분야 종사자라면 알아야 할 컴퓨터 과학에 대한 이야기",
+    },
+    {
+      title: "프로젝트",
+      href: "/category/project",
+      description: "재미삼아 진행하는 프로젝트에 대한 이야기",
     },
     {
       title: "자유로운 글",
       href: "/category/free",
-      description: "개인의 생각이나 감정을 제약 없이 표현하는 글.",
+      description: "나의 개인적인 생각을 적는 공간",
     },
   ];
 
